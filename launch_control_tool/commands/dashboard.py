337fda4864afe701572bce5b6b3f24d72ef68950
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2010-11-25 13:02:42 +0100

    Preparations for release:  * fix copyright headers to refer to launch-control-tool  * change setup.py python classifier to 4 - Beta  * change setup.py name to launch-control-tool  * change internal version to 0.2 final

--- launch_control_tool/commands/dashboard.py
+++ launch_control_tool/commands/dashboard.py
@@ -2,19 +2,19 @@
 #
 # Author: Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
 #
-# This file is part of Launch Control.
+# This file is part of launch-control-tool.
 #
-# Launch Control is free software: you can redistribute it and/or modify
+# launch-control-tool is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License version 3
 # as published by the Free Software Foundation
 #
-# Launch Control is distributed in the hope that it will be useful,
+# launch-control-tool is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with Launch Control.  If not, see <http://www.gnu.org/licenses/>.
+# along with launch-control-tool.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 Module with command-line tool commands that interact with the dashboard
