11546f85160b4d5c51c2a50be5f86e586fcd4fc5
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-04-07 09:58:28 +0200

    Bump version to 0.4.1b3

--- launch_control_tool/__init__.py
+++ launch_control_tool/__init__.py
@@ -21,7 +21,7 @@ Launch Control Tool package
 """
 
 # Chicken-and-egg problem
-__version__ = (0, 4, 1, "beta", 2)
+__version__ = (0, 4, 1, "beta", 3)
 try:
     import versiontools
     __version__ = versiontools.Version.from_tuple(__version__)
