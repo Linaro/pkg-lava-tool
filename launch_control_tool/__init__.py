016534a1ad66c17c349835704d8555a4ac8d46fb
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-04-29 12:29:52 +0200

    Bump version to 0.5 dev

--- launch_control_tool/__init__.py
+++ launch_control_tool/__init__.py
@@ -21,7 +21,7 @@ Launch Control Tool package
 """
 
 # Chicken-and-egg problem
-__version__ = (0, 4, 1, "final", 0)
+__version__ = (0, 5, 0, "dev", 0)
 try:
     import versiontools
     __version__ = versiontools.Version.from_tuple(__version__)
