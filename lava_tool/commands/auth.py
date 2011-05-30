59388c3ee3c40d961766546f36e5ac343c491f00
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-05-30 14:46:04 +1200

    skeletons of the three commands

--- lava_tool/commands/auth.py
+++ lava_tool/commands/auth.py
@@ -1,7 +1,21 @@
 from lava_tool.interface import Command
 
 
+class auth_add(Command):
+    """
+    """
+    def invoke(self):
+        pass
+
+
 class auth_list(Command):
+    """
+    """
+    def invoke(self):
+        pass
+
+
+class auth_remove(Command):
     """
     """
     def invoke(self):
