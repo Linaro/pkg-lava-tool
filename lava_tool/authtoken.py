e2fb1a84020f328b5da963dc185bb52386a62c9c
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-05-31 16:19:20 +1200

    some wibbling

--- lava_tool/authtoken.py
+++ lava_tool/authtoken.py
@@ -1,7 +1,17 @@
+import urllib
 import xmlrpclib
 
 
 class AuthenticatingServerProxy(xmlrpclib.ServerProxy):
 
-    def __init__(self):
-        pass
+    def __init__(self, uri, transport=None, encoding=None, verbose=0,
+                 allow_none=0, use_datetime=0, auth_backend=None):
+        type, uri = urllib.splittype(uri)
+        host, handler = urllib.splithost(uri)
+        user, host = urllib.splituser(host)
+        token = auth_backend.get_token_for_host()
+        if token:
+            host = 
+        xmlrpclib.ServerProxy.__init__(
+            self, transport, encoding, verbose, allow_none, use_datetime)
+        self.auth_backend = auth_backend
