ba9389a7ac798292bb452d3fa6e5bad8639e0306
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-06-01 15:31:43 +1200

    report an error if a username is supplied but no token is found

--- lava_tool/authtoken.py
+++ lava_tool/authtoken.py
@@ -1,18 +1,19 @@
 import urllib
 import xmlrpclib
 
+from lava_tool.interface import LavaCommandError
+
 
 def add_token_to_uri(uri, auth_backend):
     orig_uri = uri
     type, uri = urllib.splittype(uri)
     host, handler = urllib.splithost(uri)
     user, host = urllib.splituser(host)
-    if user:
-        token = auth_backend.get_token_for_host(user, host)
-    else:
-        token = None
-    if token is None:
+    if not user:
         return orig_uri
+    token = auth_backend.get_token_for_host(user, host)
+    if token is None:
+        raise LavaCommandError("username provided but no token found")
     return orig_uri
 
 
--- lava_tool/tests/test_authtoken.py
+++ lava_tool/tests/test_authtoken.py
@@ -4,6 +4,7 @@
 from unittest import TestCase
 
 from lava_tool.authtoken import add_token_to_uri
+from lava_tool.interface import LavaCommandError
 
 class StubAuthBackend(object):
 
@@ -15,10 +16,19 @@ class StubAuthBackend(object):
     def get_token_for_host(self, username, host):
         return self._tokens.get((username, host))
 
+
 class TestAddTokenToUri(TestCase):
 
     def test_no_username_no_token(self):
         auth_backend = StubAuthBackend([])
         uri = 'https://example.com/'
         self.assertEquals(uri, add_token_to_uri(uri, auth_backend))
+
+    def test_username_no_token_error(self):
+        # XXX Is having add_token_to_uri raise LavaCommandError too-tight
+        # coupling?
+        auth_backend = StubAuthBackend([])
+        uri = 'https://user@example.com/'
+        self.assertRaises(
+            LavaCommandError, add_token_to_uri, uri, auth_backend)
 
