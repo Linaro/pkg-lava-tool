3e2b282bddabd96e435a6b428dc0c146ed49b669
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-06-08 15:01:07 +1200

    copyright headers

--- lava_tool/tests/test_authtoken.py
+++ lava_tool/tests/test_authtoken.py
@@ -1,5 +1,20 @@
-"""
-"""
+# Copyright (C) 2011 Linaro Limited
+#
+# Author: Michael Hudson-Doyle <michael.hudson@linaro.org>
+#
+# This file is part of lava-tool.
+#
+# lava-tool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation
+#
+# lava-tool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lava-tool.  If not, see <http://www.gnu.org/licenses/>.
 
 import base64
 from unittest import TestCase
