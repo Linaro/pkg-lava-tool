15ebd8f7641dbfb3759f30c82a1649f2186d3120
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-06-08 14:30:32 +1200

    a bunch more tests

--- lava_tool/tests/test_auth_commands.py
+++ lava_tool/tests/test_auth_commands.py
@@ -3,9 +3,10 @@
 
 import StringIO
 import sys
+import xmlrpclib
 
 from lava_tool.authtoken import MemoryAuthBackend
-from lava_tool.mocker import *#ARGS, KWARGS, CONTAINS, MockerTestCase
+from lava_tool.mocker import ARGS, KWARGS, CONTAINS, MockerTestCase
 from lava_tool.interface import LavaCommandError
 from lava_tool.commands.auth import auth_add
 
@@ -63,11 +64,29 @@ class AuthAddTests(MockerTestCase):
             'TOKEN', auth_backend.get_token_for_host('user', 'example.com'))
 
     def test_token_file_and_in_url_conflict(self):
+        mocked_getuser = self.mocker.replace('getpass.getuser', passthrough=False)
+        mocked_getuser()
+        self.mocker.result("user")
+        self.mocker.replay()
         auth_backend = MemoryAuthBackend([])
         cmd = self.make_command(
-            auth_backend, HOST='http://user:TOKEN@example.com', no_check=True,
+            auth_backend, HOST='http://example.com', no_check=True,
             token_file=StringIO.StringIO('TOKEN'))
-        self.assertRaises(LavaCommandError, cmd.invoke)
+        cmd.invoke()
+        self.assertEqual(
+            'TOKEN', auth_backend.get_token_for_host('user', 'example.com'))
+
+    def test_user_taken_from_getpass(self):
+        mocked_getpass = self.mocker.replace('getpass.getpass', passthrough=False)
+        mocked_getpass(CONTAINS('Paste token'))
+        self.mocker.result("TOKEN")
+        self.mocker.replay()
+        auth_backend = MemoryAuthBackend([])
+        cmd = self.make_command(
+            auth_backend, HOST='http://user@example.com', no_check=True)
+        cmd.invoke()
+        self.assertEqual(
+            'TOKEN', auth_backend.get_token_for_host('user', 'example.com'))
 
     def test_port_included(self):
         auth_backend = MemoryAuthBackend([])
@@ -93,3 +112,33 @@ class AuthAddTests(MockerTestCase):
         cmd.invoke()
         self.assertEqual(
             'TOKEN', auth_backend.get_token_for_host('user', 'example.com:1234'))
+
+    def test_check_auth_failure_reported_nicely(self):
+        mocked_AuthenticatingServerProxy = self.mocker.replace(
+            'lava_tool.authtoken.AuthenticatingServerProxy', passthrough=False)
+        mocked_sp = mocked_AuthenticatingServerProxy(ARGS, KWARGS)
+        # nospec() is required because of
+        # https://bugs.launchpad.net/mocker/+bug/794351
+        self.mocker.nospec()
+        mocked_sp.system.whoami()
+        self.mocker.throw(xmlrpclib.ProtocolError('', 401, '', []))
+        self.mocker.replay()
+        auth_backend = MemoryAuthBackend([])
+        cmd = self.make_command(
+            auth_backend, HOST='http://user:TOKEN@example.com:1234', no_check=False)
+        self.assertRaises(LavaCommandError, cmd.invoke)
+
+    def test_check_other_failure_just_raised(self):
+        mocked_AuthenticatingServerProxy = self.mocker.replace(
+            'lava_tool.authtoken.AuthenticatingServerProxy', passthrough=False)
+        mocked_sp = mocked_AuthenticatingServerProxy(ARGS, KWARGS)
+        # nospec() is required because of
+        # https://bugs.launchpad.net/mocker/+bug/794351
+        self.mocker.nospec()
+        mocked_sp.system.whoami()
+        self.mocker.throw(xmlrpclib.ProtocolError('', 500, '', []))
+        self.mocker.replay()
+        auth_backend = MemoryAuthBackend([])
+        cmd = self.make_command(
+            auth_backend, HOST='http://user:TOKEN@example.com:1234', no_check=False)
+        self.assertRaises(xmlrpclib.ProtocolError, cmd.invoke)
