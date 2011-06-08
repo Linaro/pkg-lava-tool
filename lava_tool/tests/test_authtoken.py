ce3e07f7861dd6bb1d42d27baa9c419c352715fb
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-06-08 13:12:18 +1200

    start to resurrect tests

--- lava_tool/tests/test_authtoken.py
+++ lava_tool/tests/test_authtoken.py
@@ -3,38 +3,13 @@
 
 from unittest import TestCase
 
-from lava_tool.authtoken import add_token_to_uri
+from lava_tool.authtoken import AuthenticatingTransportMixin
 from lava_tool.interface import LavaCommandError
 
-class StubAuthBackend(object):
 
-    def __init__(self, user_host_token_list):
-        self._tokens = {}
-        for user, host, token in user_host_token_list:
-            self._tokens[(user, host)] = token
+class TestAuthenticatingTransportMixin(TestCase):
 
-    def get_token_for_host(self, username, host):
-        return self._tokens.get((username, host))
-
-
-class TestAddTokenToUri(TestCase):
-
-    def test_no_username_no_token(self):
-        auth_backend = StubAuthBackend([])
-        uri = 'https://example.com/'
-        self.assertEquals(uri, add_token_to_uri(uri, auth_backend))
-
-    def test_username_no_token_error(self):
-        # XXX Is having add_token_to_uri raise LavaCommandError too-tight
-        # coupling?
-        auth_backend = StubAuthBackend([])
-        uri = 'https://user@example.com/'
-        self.assertRaises(
-            LavaCommandError, add_token_to_uri, uri, auth_backend)
-
-    def test_adds_token(self):
-        auth_backend = StubAuthBackend([('user', 'example.com', 'TOKEN')])
-        self.assertEquals(
-            'https://user@example.com/',
-            add_token_to_uri('https://user:TOKEN@example.com/', auth_backend))
-
+    def test_no_user_no_auth(self):
+        a = AuthenticatingTransportMixin()
+        _, headers, _ = a.get_host_info('example.com')
+        self.assertEqual(None, headers)
