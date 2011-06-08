6af82d75285b475a23e0f1179954f7e8c20d4b97
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-06-08 13:06:29 +1200

    remove some crap

--- lava_tool/commands/auth.py
+++ lava_tool/commands/auth.py
@@ -1,7 +1,6 @@
 import argparse
 import getpass
 import urlparse
-import sys
 import xmlrpclib
 
 from lava_tool.authtoken import (
@@ -24,6 +23,7 @@ def normalize_xmlrpc_url(uri):
 
 class auth_add(Command):
     """
+    Add an authentication token.
     """
 
     def __init__(self, parser, args, auth_backend=None):
@@ -44,7 +44,9 @@ class auth_add(Command):
             help="Read the secret from here rather than prompting for it.")
         parser.add_argument(
             "--no-check", action='store_true',
-            help="XXX.")
+            help=("By default, a call to the remote server is made to check "
+                  "that the added token works before remembering it.  "
+                  "Passing this option prevents this check."))
 
     def invoke(self):
         uri = normalize_xmlrpc_url(self.args.HOST)
@@ -64,7 +66,7 @@ class auth_add(Command):
         if self.args.token_file:
             if parsed_host.password:
                 raise LavaCommandError(
-                    "token specified in url but --token-file also passed")
+                    "Token specified in url but --token-file also passed.")
             else:
                 token = self.args.token_file.read()
         else:
@@ -81,43 +83,15 @@ class auth_add(Command):
                 token_user = sp.system.whoami()
             except xmlrpclib.ProtocolError as ex:
                 if ex.errcode == 401:
-                    raise LavaCommandError("token rejected by server for user %s" % username)
+                    raise LavaCommandError(
+                        "Token rejected by server for user %s." % username)
                 else:
                     raise
-            if token_user is None or token_user != username:
-                raise LavaCommandError("???")
+            if token_user != username:
+                raise LavaCommandError(
+                    "whoami() returned %s rather than expected %s -- this is "
+                    "a bug." % (token_user, username))
 
         self.auth_backend.add_token(username, host, token)
 
-        print 'token added successfully for user %s' % username
-
-
-class auth_list(Command):
-    """
-    """
-    def invoke(self):
-        pass
-
-
-class auth_remove(Command):
-    """
-    """
-    def invoke(self):
-        pass
-
-
-class test_api(Command):
-
-    def get_authenticated_server_proxy(self, uri):
-        return AuthenticatingServerProxy(
-            uri, auth_backend=KeyringAuthBackend())
-
-    @classmethod
-    def register_arguments(cls, parser):
-        super(test_api, cls).register_arguments(parser)
-        parser.add_argument(
-            "HOST",
-            help=("XXX."))
-
-    def invoke(self):
-        print self.get_authenticated_server_proxy(self.args.HOST).scheduler.test()
+        print 'Token added successfully for user %s.' % username
