20a42329ae9f1283e2cdcc6edf5be98b3aeca69a
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-06-17 15:07:38 +0200

    Remove unused import

--- lava_tool/commands/auth.py
+++ lava_tool/commands/auth.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with lava-tool.  If not, see <http://www.gnu.org/licenses/>.
 
-import argparse
 import getpass
 import urlparse
 import xmlrpclib
