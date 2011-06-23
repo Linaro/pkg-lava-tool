ca20a3edf8b5ee5536c9ed573cdaa3c485f981e3
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-06-23 12:20:54 +0200

    Be pip friendly

--- setup.py
+++ setup.py
@@ -19,20 +19,11 @@
 # along with lava-tool.  If not, see <http://www.gnu.org/licenses/>.
 
 from setuptools import setup, find_packages
-try:
-    import versiontools
-except ImportError:
-    print "This package requires python-versiontools to be configured"
-    print "See: http://packages.python.org/versiontools/installation.html"
-    raise
-
-
-import lava_tool
 
 
 setup(
     name='lava-tool',
-    version=versiontools.format_version(lava_tool.__version__),
+    version=":versiontools:lava_tool:__version__",
     author="Zygmunt Krynicki",
     author_email="zygmunt.krynicki@linaro.org",
     packages=find_packages(),
@@ -60,7 +51,7 @@ setup(
         'argparse >= 1.1',
         'keyring'],
     setup_requires=[
-        'versiontools >= 1.1'],
+        'versiontools >= 1.3.1'],
     tests_require=[
         'mocker >= 1.0'],
     zip_safe=True)
