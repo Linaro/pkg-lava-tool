dc4e416a8835be834d385a194d6f95c7cf0885c7
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-12-13 17:19:52 +0100

    Add LavaNonLegacyDispatcher, simplify LavaDispatcher

--- lava_tool/dispatcher.py
+++ lava_tool/dispatcher.py
@@ -20,61 +20,36 @@
 Module with LavaDispatcher - the command dispatcher
 """
 
-import argparse
-import pkg_resources
-import sys
-
-from lava_tool.interface import LavaCommandError
+from lava_tool.interface import LavaCommandError, BaseDispatcher 
 
 
-class LavaDispatcher(object):
+class LavaDispatcher(BaseDispatcher):
     """
     Class implementing command line interface for launch control
     """
 
     toolname = None
-    description = None
-    epilog = None
 
     def __init__(self):
-        # XXX The below needs to allow some customization.
-        parser_args = dict(add_help=True)
-        if self.description is not None:
-            parser_args['description'] = self.description
-        if self.epilog is not None:
-            parser_args['epilog'] = self.epilog
-        self.parser = argparse.ArgumentParser(**parser_args)
-        self.subparsers = self.parser.add_subparsers(
-                title="Sub-command to invoke")
+        super(LavaDispatcher, self).__init__()
         prefixes = ['lava_tool']
         if self.toolname is not None:
             prefixes.append(self.toolname)
         for prefix in prefixes:
-            for entrypoint in pkg_resources.iter_entry_points(
-                "%s.commands" % prefix):
-                self.add_command_cls(entrypoint.load())
+            self.import_commands("%s.commands" % prefix)
 
-    def add_command_cls(self, command_cls):
-        sub_parser = self.subparsers.add_parser(
-            command_cls.get_name(),
-            help=command_cls.get_help(),
-            epilog=command_cls.get_epilog())
-        sub_parser.set_defaults(command_cls=command_cls)
-        sub_parser.set_defaults(sub_parser=sub_parser)
-        command_cls.register_arguments(sub_parser)
 
-    def dispatch(self, raw_args=None):
-        args = self.parser.parse_args(raw_args)
-        command = args.command_cls(self.parser, args)
-        try:
-            command.reparse_arguments(args.sub_parser, raw_args)
-        except NotImplementedError:
-            pass
-        try:
-            return command.invoke()
-        except LavaCommandError as ex:
-            print >> sys.stderr, "ERROR: %s" % (ex,)
-            return 1
+class LavaNonLegacyDispatcher(BaseDispatcher):
+    """
+    A dispatcher that wants to load only top-level commands,
+    not everything-and-the-kitchen-sink into one flat namespace
+
+    Available as `lava` command from shell
+    """
+
+    def __init__(self):
+        super(LavaNonLegacyDispatcher, self).__init__()
+        self.import_commands('lava.commands')
 
 
 def run_with_dispatcher_class(cls):
@@ -83,3 +58,7 @@ def run_with_dispatcher_class(cls):
 
 def main():
     run_with_dispatcher_class(LavaDispatcher)
+
+
+def main_nonlegacy():
+    run_with_dispatcher_class(LavaNonLegacyDispatcher)
