360d50a5f0849a1a34e4d990ce6243373caad263
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2012-03-13 17:14:36 +0100

    Move LavaCommandError to lava.tool.errors

---
+++ lava/tool/errors.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2010, 2011, 2012 Linaro Limited
+#
+# Author: Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
+#
+# This file is part of lava-tool.
+#
+# lava-tool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation
+#
+# lava-tool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lava-tool.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+lava.tool.errors
+================
+
+Error classes for LAVA Tool.
+"""
+
+class LavaCommandError(Exception):
+    """
+    Raise this from a Command's invoke() method to display an error nicely.
+
+    lava-tool will exit with a status of 1 if this is raised.
+    """
