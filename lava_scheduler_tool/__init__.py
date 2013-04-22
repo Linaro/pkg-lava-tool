e2550de9b661fad8d93b750f9f94cc72c626e0d0
Antonio Terceiro <antonio.terceiro@linaro.org>
2013-04-22 14:23:45 -0300

    Absorb lava-scheduler-tool

--- lava_scheduler_tool/__init__.py
+++ lava_scheduler_tool/__init__.py
@@ -1,27 +1,19 @@
 # Copyright (C) 2010, 2011 Linaro Limited
 #
-# Author: Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
 # Author: Michael Hudson-Doyle <michael.hudson@linaro.org>
 #
-# This file is part of lava-tool.
+# This file is part of lava-scheduler-tool.
 #
-# lava-tool is free software: you can redistribute it and/or modify
+# lava-scheduler-tool is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License version 3
 # as published by the Free Software Foundation
 #
-# lava-tool is distributed in the hope that it will be useful,
+# lava-scheduler-tool is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with lava-tool.  If not, see <http://www.gnu.org/licenses/>.
+# along with lava-scheduler-tool.  If not, see <http://www.gnu.org/licenses/>.
 
-"""
-lava.tool
-=========
-
-Generic code for command line utilities for LAVA
-"""
-
-__version__ = (0, 7, 0, "final", 0)
+__version__ = (0, 6, 0, "dev", 0)
