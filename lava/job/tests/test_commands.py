89a50e475605d6d3d7fc8dc45d99b1047f80a8e6
Milo Casagrande <milo@ubuntu.com>
2013-06-14 11:54:51 +0200

    Refactored tests, added structure for run tests.

--- lava/job/tests/test_commands.py
+++ lava/job/tests/test_commands.py
@@ -20,74 +20,111 @@
 Unit tests for the commands classes
 """
 
-from argparse import ArgumentParser
 import json
-from os import (
-    makedirs,
-    removedirs,
-)
-from os.path import(
-    exists,
-    join,
-)
-from shutil import(
-    rmtree,
-)
-from tempfile import mkdtemp
+import os
+import shutil
+import sys
+import tempfile
+import argparse
+
+from mock import MagicMock
 from unittest import TestCase
 
-from lava.config import NonInteractiveConfig
-from lava.job.commands import *
+from lava.config import NonInteractiveConfig, Parameter
+
+from lava.job.commands import (
+    new,
+)
+
 from lava.tool.errors import CommandError
 
-from mocker import Mocker
 
 def make_command(command, *args):
-    parser = ArgumentParser(description="fake argument parser")
+    parser = argparse.ArgumentParser(description="fake argument parser")
     command.register_arguments(parser)
     the_args = parser.parse_args(*args)
     cmd = command(parser, the_args)
     cmd.config = NonInteractiveConfig({ 'device_type': 'foo', 'prebuilt_image': 'bar' })
     return cmd
 
+
 class CommandTest(TestCase):
 
     def setUp(self):
-        self.tmpdir = mkdtemp()
+        # Fake the stdout.
+        self.original_stdout = sys.stdout
+        sys.stdout = open("/dev/null", "w")
+        self.original_stderr = sys.stderr
+        sys.stderr = open("/dev/null", "w")
+        self.original_stdin = sys.stdin
+
+        self.device = "panda02"
+
+        self.tmpdir = tempfile.mkdtemp()
+        self.tmpfile = tempfile.NamedTemporaryFile(delete=False)
+        self.parser = MagicMock()
+        self.args = MagicMock()
+        self.args.interactive = MagicMock(return_value=False)
+        self.args.FILE = self.tmpfile.name
+
+        self.device_type = Parameter('device_type')
+        self.prebuilt_image = Parameter('prebuilt_image',
+                                        depends=self.device_type)
+        self.config = NonInteractiveConfig(
+            {'device_type': 'foo', 'prebuilt_image': 'bar'})
+        # self.config.put_parameter(self.device_type, 'foo')
+        # self.config.put_parameter(self.prebuilt_image, 'bar')
 
     def tearDown(self):
-        rmtree(self.tmpdir)
+        sys.stdin = self.original_stdin
+        sys.stdout = self.original_stdout
+        sys.stderr = self.original_stderr
+        os.unlink(self.tmpfile.name)
+        shutil.rmtree(self.tmpdir)
 
     def tmp(self, filename):
-        return join(self.tmpdir, filename)
+        return os.path.join(self.tmpdir, filename)
+
 
 class JobNewTest(CommandTest):
 
+    def setUp(self):
+        super(JobNewTest, self).setUp()
+        self.args.FILE = self.tmp("new_file.json")
+        self.new_command = new(self.parser, self.args)
+        self.new_command.config = self.config
+
+    def tearDown(self):
+        super(JobNewTest, self).tearDown()
+        if os.path.exists(self.args.FILE):
+            os.unlink(self.args.FILE)
+
     def test_create_new_file(self):
-        f = self.tmp('file.json')
-        command = make_command(new, [f])
-        command.invoke()
-        self.assertTrue(exists(f))
+        self.new_command.invoke()
+        self.assertTrue(os.path.exists(self.args.FILE))
 
     def test_fills_in_template_parameters(self):
-        f = self.tmp('myjob.json')
-        command = make_command(new, [f])
-        command.invoke()
+        self.new_command.invoke()
 
-        data = json.loads(open(f).read())
+        data = json.loads(open(self.args.FILE).read())
         self.assertEqual(data['device_type'], 'foo')
 
-    def test_wont_overwriteexisting_file(self):
-        existing = self.tmp('existing.json')
-        with open(existing, 'w') as f:
+    def test_wont_overwrite_existing_file(self):
+        with open(self.args.FILE, 'w') as f:
             f.write("CONTENTS")
-        command = make_command(new, [existing])
-        with self.assertRaises(CommandError):
-            command.invoke()
-        self.assertEqual("CONTENTS", open(existing).read())
+
+        self.assertRaises(CommandError, self.new_command.invoke)
+        self.assertEqual("CONTENTS", open(self.args.FILE).read())
+
 
 class JobSubmitTest(CommandTest):
 
     def test_receives_job_file_in_cmdline(self):
         cmd = make_command(new, ['FOO.json'])
         self.assertEqual('FOO.json', cmd.args.FILE)
+
+
+class JobRunTest(CommandTest):
+
+    def test_invoke_raises(self):
+        pass
