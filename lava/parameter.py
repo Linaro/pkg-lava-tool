5fc5328bd4a547d8a62eeca3165642df57d5c9aa
Milo Casagrande <milo@ubuntu.com>
2013-06-20 15:12:00 +0200

    Refactored Parameter class.
        * Moved Parameter class in its own file.
        * Added value attribute.

---
+++ lava/parameter.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2013 Linaro Limited
+#
+# Author: Antonio Terceiro <antonio.terceiro@linaro.org>
+#
+# This file is part of lava-tool.
+#
+# lava-tool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation
+#
+# lava-tool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lava-tool.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Parameter class and its accessory methods/functions.
+"""
+
+
+class Parameter(object):
+    """A parameter with an optional dependency."""
+    def __init__(self, id, value=None, depends=None):
+        self.id = id
+        self.value = value
+        self.depends = depends
