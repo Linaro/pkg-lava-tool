cde1f30cd27eb21f7e4ce0124f3ff6beebcd1c55
Milo Casagrande <milo@ubuntu.com>
2013-06-27 16:30:20 +0200

    Added structure for testdef commands.

---
+++ lava/testdef/commands.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2013 Linaro Limited
+#
+# Author: Milo Casagrande <milo.casagrande@linaro.org>
+#
+# This file is part of lava-tool.
+#
+# lava-tool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation
+#
+# lava-tool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lava-tool.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Test definition commands class.
+"""
+
+from lava.tool.command import CommandGroup
+
+
+class testdef(CommandGroup):
+    """LAVA devices handling."""
+
+    namespace = "lava.testdef.commands"
