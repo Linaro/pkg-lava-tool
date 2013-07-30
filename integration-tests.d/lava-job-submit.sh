961a02ab1ab3330838578ae2b42b5f3f96c2b62a
Milo Casagrande <milo@ubuntu.com>
2013-07-30 19:42:36 +0200

    Corrected server/rpc_endpoint usage.

--- integration-tests.d/lava-job-submit.sh
+++ integration-tests.d/lava-job-submit.sh
@@ -2,11 +2,11 @@ fixed_response 999
 
 lava_config <<CONFIG
 [DEFAULT]
-server = http://localhost:5000
+server = validation.example.com
 
 [server=validation.example.com]
-rpc_endpoint = /ok
+rpc_endpoint = http://localhost:5000/ok
 CONFIG
 
-lava job submit integration-tests.d/sample/nexus.json > $tmpdir/output
+lava job submit integration-tests.d/sample/nexus.json -n > $tmpdir/output
 grep "Job submitted with job ID 999" $tmpdir/output
